884c6fbe6207cb96557e7053a29774617e952e1a
Babi <babak572002@yahoo.com>
2021-03-25 23:25:01 +0800

    Add Template

--- DevCardMVC/Controllers/HomeController.cs
+++ DevCardMVC/Controllers/HomeController.cs
@@ -1,21 +1,17 @@
 ﻿using DevCardMVC.Models;
 using Microsoft.AspNetCore.Mvc;
-using Microsoft.Extensions.Logging;
-using System;
-using System.Collections.Generic;
 using System.Diagnostics;
-using System.Linq;
-using System.Threading.Tasks;
+
 
 namespace DevCardMVC.Controllers
 {
     public class HomeController : Controller
     {
-        private readonly ILogger<HomeController> _logger;
+       
 
-        public HomeController(ILogger<HomeController> logger)
+        public HomeController()
         {
-            _logger = logger;
+           
         }
 
         public IActionResult Index()
@@ -23,7 +19,7 @@ namespace DevCardMVC.Controllers
             return View();
         }
 
-        public IActionResult Privacy()
+        public IActionResult Contact()
         {
             return View();
         }
