c3e3d918f809d2ceb6fabbc9e8b855da8e498f05
Babi <babak572002@yahoo.com>
2021-03-26 14:46:33 +0800

    Model

---
+++ DevCardMVC/Models/Article.cs
@@ -0,0 +1,12 @@
+﻿namespace DevCardMVC.Models
+{
+    public class Article
+    {
+        public long Id { get; set; }
+        public string Title { get; set; }
+        public string Description { get; set; }
+        public string Image { get; set; }
+
+       
+    }
+}
---
+++ DevCardMVC/Models/Project.cs
@@ -0,0 +1,13 @@
+﻿namespace DevCardMVC.Models
+{
+    public class Project
+    {
+        public long Id { get; set; }
+        public string Name { get; set; }
+        public string Description { get; set; }
+        public string Image { get; set; }
+        public string Client { get; set; }
+
+   
+    }
+}
